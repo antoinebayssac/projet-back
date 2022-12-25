42a1a6abb526ca7b967ae9ac224530b68c18d53c
BARDAT Ryan <105910237+REIG3N@users.noreply.github.com>
2022-12-25 10:43:07 +0100

    login register terminée

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/filmProfile', '\App\Http\Controllers\PageController@filmProfile');
+Route::get('/albums', '\App\Http\Controllers\PageController@albums');
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
