e81a6d4fe29fdcf4718fe612f1f2171beb5ae588
antoinebayssac <antoine.bayssac@edu.devinci.fr>
2023-01-03 20:44:07 +0100

    :dizzy: add some tailswind css of singlepage for films

--- singlepage.php
+++ singlepage.php
@@ -15,10 +15,9 @@ require_once 'ressources/connection.php';?>
 
 <?php require_once'./ressources/navbar.php'?>
 
-<div class="container_singlemovie">
+<div class="container_singlemovie flex flex-row-reverse">
 
 </div>
-
 
 
 <script src="https://unpkg.com/@themesberg/flowbite@latest/dist/flowbite.bundle.js"></script>
@@ -33,17 +32,33 @@ require_once 'ressources/connection.php';?>
         .then(movie => {
             console.log(movie)
             let card = document.createElement("div")
-            card.classList = "flex"
+            card.classList = "w-1/2 flex justify-center h-auto p-8 drop-shadow-xl"
+            let info = document.createElement("div")
+            info.classList = "w-1/2 flex justify-center flex-col pr-20"
+            container_single.appendChild(info)
             container_single.appendChild(card)
             let img = document.createElement("img")
+            img.classList = "w-80 min-w-40 h-auto rounded-md"
             img.src="https://image.tmdb.org/t/p/w500" + movie["poster_path"]
             let title = document.createElement("h1")
             title.innerHTML = movie["title"]
+            let lang = document.createElement("h2")
+            lang.innerHTML = movie["original_language"]
+            let desc = document.createElement("p")
+            desc.innerHTML = movie["overview"]
+            let date = document.createElement("p")
+            date.innerHTML = movie["release_date"]
+            let page = document.createElement("p")
+            page.innerHTML = movie["homepage"] 
+            let genre = document.createElement("h3")
+            genre.innerHTML = movie["genre"] 
             card.appendChild(img)
-            card.appendChild(title)
-            
-            
-
+            info.appendChild(title)
+            info.appendChild(genre)
+            info.appendChild(date)
+            info.appendChild(lang)
+            info.appendChild(page)
+            info.appendChild(desc)
         }) 
 </script>   
 </body>
