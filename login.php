9dbc3a826cd0e39af68e0b5f984aaf95db6985ee
antoinebayssac <antoine.bayssac@edu.devinci.fr>
2023-01-04 21:52:05 +0100

    :lipstick: modification of css and :hammer: add some code for SESSION login and register

--- login.php
+++ login.php
@@ -4,30 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="build.css">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Login</title>
+    <title>Connexion</title>
 </head>
 <body>
 
-    <section class="bg-gray-50 dark:bg-gray-900">
+
+
+
+    <section class="bg-neutral-800">
+
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-            <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
-            Flowbite    
-        </a>
-        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <nav class="  px-2 sm:px-4 py-2.5">
+            <div class="container flex flex-wrap items-center justify-between mx-auto">
+                <a href="index.php" class="flex items-center">
+                    <img src="IMG/WEEK'S%20FiLMS.png" class="h-6 mr-3 sm:h-9" alt="Logo" />
+                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Week's Films</span>
+                </a>
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0  dark:border-gray-700">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Sign in to your account
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-blue-900 md:text-2xl ">
+                    Connexion
                 </h1>
                 <form class="space-y-4 md:space-y-6" method="POST">
                     <div>
-                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                        <label for="email" class="block mb-2 text-sm font-medium text-blue-700 ">Votre email</label>
+                        <input type="email" name="email" id="email" class="bg-blue-700 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
                     </div>
+
+
                     <div>
-                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                        <label for="password" class="block mb-2 text-sm font-medium text-blue-700 ">Mot de passe</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-blue-700 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                     </div>
                     <div class="flex items-center justify-between">
                         <div class="flex items-start">
@@ -35,14 +44,14 @@
                                 <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
                             </div>
                             <div class="ml-3 text-sm">
-                                <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                                <label for="remember" class="text-blue-500 dark:text-blue-300">Se Souvenir de moi</label>
                             </div>
                         </div>
-                        <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                        <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Mot de passe oublié ?</a>
                     </div>
-                    <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                        Don’t have an account yet? <a href="register.php" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                    <button type="submit" class="w-full text-blue-500 bg-primary-600 border-solid border-black hover:bg-primary-700 focus:ring-4  focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Se Connecter</button>
+                    <p class="text-sm font-light text-gray-500 dark:text-blue-400">
+                        Vous n'êtes pas inscrit ? <a href="register.php" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Inscription</a>
                     </p>
                 </form>
             </div>
@@ -63,19 +72,21 @@ if ($_POST) {
         $result = $connection->connect($email, $password);
 
         if ($result) {
+            $info = $connection->getinfouser($email);
+            $_SESSION["last_name"] = $info[0];
+            $_SESSION["first_name"] = $info[1];
+            $_SESSION["email"] = $email;
             echo '<p>Connexion avec succès !</p>';
             header("Location: index.php");
-            } else {
-                header('Location: login.php');
-            }
+        } 
+        else {
+           echo '<p>Identifiant Incorrecte !</p>';
         }
-        else{
-            echo 'password invalid';
-        }
-    } else {
-
-        echo '<p> Please enter an email and a valid password !</p>';
     }
+    else{
+        echo 'password invalid';
+    }
+}
 ?>
 </body>
 </html>
