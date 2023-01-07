97d6bab28a15d8110e7de970525b857e721f15e2
antoinebayssac <antoine.bayssac@edu.devinci.fr>
2023-01-07 19:05:16 +0100

    :hammer: add informations for bdd user

--- ressources/user.php
+++ ressources/user.php
@@ -7,7 +7,9 @@ class user
         public string $password,
         public string $password2,
         public string $firstName,
-        public string $lastName
+        public string $lastName,
+        public int $age,
+        public string $description
     )
     {
     }
